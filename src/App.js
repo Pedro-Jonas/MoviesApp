227e7d64b432770751405d5fed4ffc7bc4f90009
Pedro Jonas <pedrojonas.pj77@gmail.com>
2025-06-03 10:01:29 -0300

    feat: criação do banner e container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Header/>
+      <Banner image="assistir"/>
+      <Container>
+        <h1>Conteudo</h1>
+      </Container>
       <Footer/>
     </>
   );
