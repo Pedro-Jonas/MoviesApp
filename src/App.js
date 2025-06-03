37fdb5a27676e4825159ecefd1f3b3584c9e053f
Pedro Jonas <pedrojonas.pj77@gmail.com>
2025-06-03 11:06:55 -0300

    feat: criação dos cards

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
+import Card from "./components/Card";
+import videos from "./json/db.json"
 
 function App() {
   return (
     <>
       <Header/>
-      <Banner image="assistir"/>
+      <Banner image="home"/>
       <Container>
-        <h1>Conteudo</h1>
+        <h1>Desenhos Animados</h1>
+        <section className="cards">
+          {videos.map(video =>  <Card id={video.id} key={video.id}/>)}
+        </section>
       </Container>
       <Footer/>
     </>
